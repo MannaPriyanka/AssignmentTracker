ab8c7e14c5041492c5f63c2e1c5d2918cd84c073
MannaPriyanka <mannapriyanka04@gmail.com>
2021-06-15 21:35:38 +0530

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php
- $conn = 'mysql:host=localhost;dbname=assignment_tracker';
- $username ='root';
+ $conn = 'mysql:host=remotemysql.com;dbname=TSjz7MTMUW';
+ $username ='TSjz7MTMUW';
+ $password = 'cekOr33H3n';
  error_reporting(0);
  try{
-    $db = new PDO($conn,$username);
+    $db = new PDO($conn,$username,$password);
  }catch(PDOException $e)
  {
     $error ="Database Error : ";
