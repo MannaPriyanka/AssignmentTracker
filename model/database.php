01bf21eefcda893fe14861dbcf5e0e77259f0ea0
MannaPriyanka <52463586+MannaPriyanka@users.noreply.github.com>
2021-06-15 23:04:31 +0530

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
- $conn = 'mysql:host=remotemysql.com;dbname=TSjz7MTMUW';
- $username ='TSjz7MTMUW';
- $password = 'cekOr33H3n';
+ $conn = 'mysql:host=sql100.epizy.com;dbname=epiz_28891647_assignment_tracker';
+ $username ='epiz_28891647';
+ $password = 'q4a9aSR75yGZCCz';
  error_reporting(0);
  try{
     $db = new PDO($conn,$username,$password);
